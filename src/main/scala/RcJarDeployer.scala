a3c15cbf18ccfb509447eb185d73e65e849edcff
Qin Shao Hui <qshadun@gmail.com>
2012-12-03 15:10:20 +0800

    abc

---
+++ src/main/scala/RcJarDeployer.scala
@@ -0,0 +1,14 @@
+import org.apache.commons.io.FileUtils
+import java.io.File
+
+object RcJarDeployer {
+  val jarFileName = "D:\\devl\\rc920\\onyx-commons\\onyx-commons-lwsso\\target\\onyx-commons-lwsso-1.48.jar"
+  val rcFolderName = """D:\devl\rc920\target-deployer\ccm"""
+
+  
+  def main(args: Array[String]): Unit = {
+    val jarFile = new File(jarFileName)
+    val jarName = jarFile
+  }
+
+}

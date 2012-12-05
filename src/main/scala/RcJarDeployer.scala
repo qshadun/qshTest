4f6fab8439a8368a6f714cfea914a8db32ca0e61
Qin Shao Hui <qshadun@gmail.com>
2012-12-05 10:52:49 +0800

    finish jar deploy class

--- src/main/scala/RcJarDeployer.scala
+++ src/main/scala/RcJarDeployer.scala
@@ -2,8 +2,8 @@ import org.apache.commons.io.FileUtils
 import java.io.File
 
 object RcJarDeployer {
-  val jarFileName = """C:\devl\projects\rc920\onyx-commons\onyx-commons-lwsso\target\onyx-commons-lwsso-1.48.jar"""
-  val rcFolderName = """C:\devl\projects\rc920\target-deployer\ccm"""
+  val jarFileName = """D:\devl\rc920\onyx-commons\onyx-commons-lwsso\target\onyx-commons-lwsso-1.48.jar"""
+  val rcFolderName = """D:\devl\rc920\target-deployer\ccm"""
 
   def calcTargetDirs(rcFolderName: String): List[File] = {
     val appsFolder = new File(rcFolderName, "apps")
@@ -19,6 +19,15 @@ object RcJarDeployer {
     val jarFile = new File(jarFileName)
     val jarName = jarFile.getName()
     
+    val targetDirs = calcTargetDirs(rcFolderName)
+    targetDirs map {folder =>
+      val f = new File(folder, jarName)
+      if (f.exists) {
+        FileUtils.copyFile(jarFile, f, true)
+        println("Source file is: " + jarFileName)
+        println("Copied to " + f.getAbsolutePath())
+      }
+    }
     
   }
 
